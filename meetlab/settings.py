20907971d0a73185691bf6df1a2d87ae69a2841e
Siraj Ali <starlordali@gmail.com>
2021-03-18 21:56:04 +0530

    Update settings.py

--- meetlab/settings.py
+++ meetlab/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'bpy895qg5o$2=@9az%*_w6*@49y9$g-r)oz+mr+xwzw^y(xal!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['142.93.220.156']
 
 
 # Application definition
@@ -137,5 +137,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'starlordali@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ammi@gmail007007'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
