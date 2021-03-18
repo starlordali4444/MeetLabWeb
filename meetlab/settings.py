37954f2336a9fd3a723485d0b855ab14170deedf
Siraj Ali <starlordali@gmail.com>
2021-03-18 23:36:58 +0530

    s

--- meetlab/settings.py
+++ meetlab/settings.py
@@ -140,9 +140,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =[
-    BASE_DIR/ "static",
-]
+# STATICFILES_DIRS =[
+#     BASE_DIR/ "static",
+# ]
 
 STATIC_ROOT = BASE_DIR/ "static/"
 
