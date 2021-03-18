9030cdbd258bebfe765c6bfea412c2743a628830
Siraj Ali <starlordali@gmail.com>
2021-03-18 22:10:40 +0530

    POstgret changes

--- meetlab/settings.py
+++ meetlab/settings.py
@@ -76,12 +76,26 @@ WSGI_APPLICATION = 'meetlab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'MeetLabWeb',
+            'USER': 'ALI',
+            'PASSWORD': 'Ali@db007',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
+
+
 
 
 # Password validation
