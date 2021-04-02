0318420fa3ec8592ba50e56c9ed3e73e5ae2f026
Siraj Ali <starlordali@gmail.com>
2021-04-02 21:18:32 +0530

    commit123

--- meetlab/settings.py
+++ meetlab/settings.py
@@ -25,11 +25,9 @@ SECRET_KEY = 'bpy895qg5o$2=@9az%*_w6*@49y9$g-r)oz+mr+xwzw^y(xal!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '68.183.83.188',
-    'meetphotolab.in',
-    'www.meetphotolab.in',
-]
+ALLOWED_HOSTS =['*']
+
+
 
 
 # Application definition
