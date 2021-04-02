06b8f49ed7e4549ae6083b103f290df5ab06c77e
Siraj Ali <starlordali@gmail.com>
2021-04-02 22:07:35 +0530

    server ip

--- meetlab/settings.py
+++ meetlab/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'bpy895qg5o$2=@9az%*_w6*@49y9$g-r)oz+mr+xwzw^y(xal!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =['*']
+ALLOWED_HOSTS =[
+    '206.189.141.248',
+    'meetphotolab.in',
+    'www.meetphotolab.in'
+]
 
 
 
